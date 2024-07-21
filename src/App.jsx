a4b54818ba8473f33d2b794b3cf90189c224c271
AshikaJubi <ashikajubi29@gmail.com>
2024-07-21 21:44:09 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavigationComponent from './Components/NavigationComponent/NavigationComponent'
+import './App.css'
+
+function App() {
+  return (
+    <NavigationComponent/>
+  )
+}
+
+export default App
